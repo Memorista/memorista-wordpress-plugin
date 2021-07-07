e9a389d9c29c5e191e18c41c3146cd816c3b63f2
Emin Khateeb <emin93@users.noreply.github.com>
2021-07-07 20:53:43 +0200

    Download Client UI via NodeJS post install script (#8)
    * now auto-fetching client-ui for local development as a post install script
    
    * replaced custom download steps with npm ci
    
    * added node cache
    
    * now skipping download when the file already exists
    
    * renamed fetch to download
    
    * minor simplification

---
+++ scripts/download-client-ui.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const Downloader = require("nodejs-file-downloader");
+
+const download = (url, fileName) => {
+    if (fs.existsSync(`./src/${fileName}`)) return;
+
+    const downloader = new Downloader({
+        url,
+        fileName,
+        directory: "./src",
+        cloneFiles: false,
+    });
+    downloader.download();
+};
+
+download("https://unpkg.com/@memorista/client-ui@1/dist/index.js", "memorista-client-ui.js");
+download("https://unpkg.com/@memorista/client-ui@1/dist/index.css", "memorista-client-ui.css");
